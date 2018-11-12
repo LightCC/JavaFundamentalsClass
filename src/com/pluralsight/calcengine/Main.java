729170aff2f4535fa3f0368073a1a54f15544aad
Mike Leiter <light_cc@yahoo.com>
2018-11-12 15:06:10 -0500

    Defect Fix (7c) - Test parameter incorrect
    * fixed subtract test parameter from 27 to 17, to match the course values.

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -10,7 +10,7 @@ public class Main {
         MathEquation[] equations = new MathEquation[4];
         equations[0] = new MathEquation('d',100.0d, 50.0d);
         equations[1] = new MathEquation('a',25.0d, 92.0d);
-        equations[2] = new MathEquation('s',225.0d, 27.0d);
+        equations[2] = new MathEquation('s',225.0d, 17.0d);
         equations[3] = new MathEquation('m',11.0d, 3.0d);
 
         for(MathEquation equation : equations) {
