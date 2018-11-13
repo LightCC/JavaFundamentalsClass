51fdc11d9fe099ee44507dff2821f7d8f7f187a0
Mike Leiter <light_cc@yahoo.com>
2018-11-13 01:28:12 -0500

    Section 13.2: Refactor to removed deprecated classes and files after DynamicHelper conversion * Remove the static CalculateHelper Class * Remove enum MathCommand list (now using dynamic specialized classes) * Remove code from deprecated functions that are commented out in Main

--- src/com/pluralsight/myapp/Main.java
+++ src/com/pluralsight/myapp/Main.java
@@ -2,8 +2,6 @@ package com.pluralsight.myapp;
 
 import com.pluralsight.calcengine.DynamicHelper;
 import com.pluralsight.calcengine.InvalidStatementException;
-import com.pluralsight.calcengine.CalculateHelper;
-import com.pluralsight.calcengine.CalculateBase;
 import com.pluralsight.calcengine.Divider;
 import com.pluralsight.calcengine.Adder;
 import com.pluralsight.calcengine.MathProcessing;
@@ -14,7 +12,6 @@ import com.pluralsight.calcengine.Multiplier;
 public class Main {
 
     public static void main(String[] args) {
-//        useCalculatorBase();
 
         String[] statements ={
                 "add 1.0",
@@ -47,31 +44,6 @@ public class Main {
                 }
             }
         }
-
-//        static void useCalculateHelper(){
-//            String[] statements = {
-//                    "add 1.0",
-//                    "add xx 25.0",
-//                    "addX 1.0 2.0",
-//                    "divide 100.0 50.0",    // 100.0 / 50.0 = 2.0
-//                    "add 25.0 92.0",        // 25.0 + 92.0 = 117.0
-//                    "subtract 225.0 17.0",  // 225.0 - 17.0 = 108.0
-//                    "multiply 11.0 3.0",    // 11.0 * 3.0 = 33.0
-//            };
-//
-//            CalculateHelper helper = new CalculateHelper();
-//            for(String statement:statements){
-//                try{
-//                    helper.process(statement);
-//                    System.out.println(helper);
-//                } catch (InvalidStatementException e){
-//                    System.out.println(e.getMessage());
-//                    if(e.getCause() != null){
-//                        System.out.println("    Original exception: " + e.getCause().getMessage());
-//                    }
-//                }
-//            }
-//        }
 
     }
 
